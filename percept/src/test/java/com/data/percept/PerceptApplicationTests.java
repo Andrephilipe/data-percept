07ad46e7279cbb0d0a87f430cfceb42e2b9fc478
André Philipe <CASSIANO.APG@GMAIL.COM>
2023-08-30 09:55:47 -0300

    Iniciando o projeto

---
+++ percept/src/test/java/com/data/percept/PerceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.percept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
