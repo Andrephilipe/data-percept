907902c308f80b3ca3990bddf30184ff0cf8a3a0
André Philipe <CASSIANO.APG@GMAIL.COM>
2023-09-09 15:25:05 -0300

    dev01

--- django-percept/meuprojeto/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Item
-
-def lista_itens(request):
-    itens = Item.objects.all()
-    return render(request, 'C:/Users/andre.p.cassiano/projeto-data-percept/data-percept/django-percept/meuprojeto/templates/lista_itens.html', {'itens': itens})
